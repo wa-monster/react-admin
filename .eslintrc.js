45ccb579a728cdc327bf283ad2050f64270d7ea4
wa-monster <32135399+wa-monster@users.noreply.github.com>
2023-10-13 22:34:35 +0800

    feat: eslint忽略config-overrides

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,48 +1,44 @@
 module.exports = {
-    // 为我们提供运行环境，一个环境定义了一组预定义的全局变量
-    "env": {
-        "browser": true,
-        "es2021": true
+  // 为我们提供运行环境，一个环境定义了一组预定义的全局变量
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parser": "@typescript-eslint/parser",
-    // ESLint 默认使用Espree作为其解析器，你可以在配置文件中指定一个不同的解析器
-    // "parser": "@typescript-eslint/parser",
-    // 配置解析器支持的语法
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+  ],
+  ignorePatterns: ["config-overrides.js"],
+  parser: "@typescript-eslint/parser",
+  // ESLint 默认使用Espree作为其解析器，你可以在配置文件中指定一个不同的解析器
+  // "parser": "@typescript-eslint/parser",
+  // 配置解析器支持的语法
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  // ESLint 支持使用第三方插件。在使用插件之前，你必须使用 npm 安装它。
+  // 在配置文件里配置插件时，可以使用 plugins 关键字来存放插件名字的列表。插件名称可以省略 eslint-plugin- 前缀。
+  plugins: ["@typescript-eslint", "react"],
+  rules: {
+    semi: 2,
+  },
+  settings: {
+    react: {
+      program: "React",
+      version: "detect",
     },
-     // ESLint 支持使用第三方插件。在使用插件之前，你必须使用 npm 安装它。
-    // 在配置文件里配置插件时，可以使用 plugins 关键字来存放插件名字的列表。插件名称可以省略 eslint-plugin- 前缀。
-    "plugins": [
-        "@typescript-eslint",
-        "react"
-    ],
-    "rules": {
-        semi: 2
-    },
-    "settings":{
-        react:{
-            program:"React",
-            version:"detect"
-        }
-    }
+  },
 };
--- config-overrides.js
+++ config-overrides.js
@@ -1,51 +1,60 @@
-const { override,addLessLoader ,addWebpackAlias, addWebpackPlugin,adjustStyleLoaders } = require('customize-cra');
-const CompressionWebpackPlugin = require('compression-webpack-plugin') // gzip压缩, 可以压缩js css 
-const {BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
-const path = require('path');
+const {
+  override,
+  addLessLoader,
+  addWebpackAlias,
+  addWebpackPlugin,
+  adjustStyleLoaders,
+} = require("customize-cra");
+const CompressionWebpackPlugin = require("compression-webpack-plugin"); // gzip压缩, 可以压缩js css
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const path = require("path");
 
 //配置开发模式和打包模式
-const addCustom = ()=>{
-    return (config)=>{
-        if(process.env.NODE_ENV === 'production'){
-            config.devtool = false //去除map文件
-            config.plugins = [...config.plugins,new CompressionWebpackPlugin({
-                test:/.js$|.css$/,
-                threshold:1024,
-            })]
-            
-            const { promisify } = require('util')
-            const chalk = require('chalk')
-            const clear = require('clear')
-            const figlet = promisify(require('figlet'))
-            clear()
-            const log = content=>console.log(chalk.blue(content))
-            figlet('YANG').then(res=>log(res))
-
-        }
-        return config
+const addCustom = () => {
+  return (config) => {
+    if (process.env.NODE_ENV === "production") {
+      config.devtool = false; //去除map文件
+      config.plugins = [
+        ...config.plugins,
+        new CompressionWebpackPlugin({
+          test: /.js$|.css$/,
+          threshold: 1024,
+        }),
+      ];
+      const { promisify } = require("util");
+      const chalk = require("chalk");
+      const clear = require("clear");
+      const figlet = promisify(require("figlet"));
+      clear();
+      const log = (content) => console.log(chalk.blue(content));
+      figlet("YANG").then((res) => log(res));
     }
-}
+    return config;
+  };
+};
 
 module.exports = override(
-    // 配置别名
-    addWebpackAlias({
-        ['@']:path.resolve(__dirname,'src')
-    }),
-    // 配置less
-    addLessLoader({
-        lessOptions: {
-            javascriptEnabled: true,
-            localIdentName: '[local]--[hash:base64:5]' // 自定义 CSS Modules 的 localIdentName
-        }
-    }),
-    adjustStyleLoaders(({ use: [, , postcss] }) => {
-        const postcssOptions = postcss.options;
-        postcss.options = { postcssOptions };
-    }),
-    addCustom(),
-    process.env.NODE_ENV === 'production'? addWebpackPlugin(
+  // 配置别名
+  addWebpackAlias({
+    ["@"]: path.resolve(__dirname, "src"),
+  }),
+  // 配置less
+  addLessLoader({
+    lessOptions: {
+      javascriptEnabled: true,
+      localIdentName: "[local]--[hash:base64:5]", // 自定义 CSS Modules 的 localIdentName
+    },
+  }),
+  adjustStyleLoaders(({ use: [, , postcss] }) => {
+    const postcssOptions = postcss.options;
+    postcss.options = { postcssOptions };
+  }),
+  addCustom(),
+  process.env.NODE_ENV === "production"
+    ? addWebpackPlugin(
         new BundleAnalyzerPlugin({
-            analyzerMode: 'static' //输出静态报告文件report.html，而不是启动一个web服务
+          analyzerMode: "static", //输出静态报告文件report.html，而不是启动一个web服务
         })
-    ) : undefined
+      )
+    : undefined
 );
