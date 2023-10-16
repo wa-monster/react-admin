aa23be6060a12c6efd8128b4038e019ddc575a0f
wa-monster <32135399+wa-monster@users.noreply.github.com>
2023-10-16 23:21:56 +0800

    feat: 使用css module

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ParticlesBg from "particles-bg";
+import styles from "./index.module.less";
 export default function Login() {
   return (
-    <div className="bg-black">
-      <ParticlesBg type="square" bg={true} />
+    <div className="login-content w-full h-full flex justify-center items-center">
+      <ParticlesBg type="fountain" bg={true} />
+      <div className={`w-[400px] h-[500px] ${styles.loginForm}`}></div>
     </div>
   );
 }
