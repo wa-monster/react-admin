9adc5a685a362d92e314c123855dff0a6af2616e
wa-monster <32135399+wa-monster@users.noreply.github.com>
2023-11-20 16:54:01 +0000

    Deploying to gh-pages from @ wa-monster/yang-admin@20cb54b1c5581743e72610e9ff35a79dba39be15 🚀

---
+++ static/js/434.ba03c9ee.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkyang_template=self.webpackChunkyang_template||[]).push([[434],{7434:function(e,n,t){t.r(n);t(3330);var a=t(2763),u=t(3726);n.default=(0,a.P)((function(){return(0,u.jsx)("div",{children:"Home"})}))}}]);
