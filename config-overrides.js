20e3004e7829c69681bd0e13984afb58d885a43b
wa-monster <32135399+wa-monster@users.noreply.github.com>
2023-12-12 00:02:08 +0800

    feat: awww

--- config-overrides.js
+++ config-overrides.js
@@ -13,11 +13,13 @@ paths.appBuild = path.join(path.dirname(paths.appBuild), "./yang-amdin.org");
 //配置开发模式和打包模式
 const addCustom = () => {
   return (config) => {
+    config.entry = "/src/a1";
     if (process.env.NODE_ENV === "production") {
       config.devtool = false; //去除map文件// 关闭sourceMap
 
       // 配置打包后的文件位置
       // config.output.path = __dirname + "/dist";
+
       config.output.publicPath = "/yang-admin/";
       config.plugins = [
         ...config.plugins,
