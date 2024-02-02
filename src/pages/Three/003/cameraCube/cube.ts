3b71cf34c25cea5bb5ff4a4fc60d4b514f4bcdd2
wa-monster <2037485086@qq.com>
2024-02-02 15:46:40 +0800

    feat: 加上环境变量

--- src/pages/Three/003/cameraCube/cube.ts
+++ src/pages/Three/003/cameraCube/cube.ts
@@ -5,7 +5,7 @@ const gltfLoader = new GLTFLoader();
 const dracoLoader = new DRACOLoader();
 const baseUrl = process.env.PUBLIC_URL;
 const loadGLTF = async () => {
-  dracoLoader.setDecoderPath("/draco/");
+  dracoLoader.setDecoderPath(baseUrl + "/draco/");
   gltfLoader.setDRACOLoader(dracoLoader);
   const cameraData = await gltfLoader.loadAsync(
     baseUrl + "/models/camera/camera.glb"
--- src/pages/Three/004/loaderGLB/cube.ts
+++ src/pages/Three/004/loaderGLB/cube.ts
@@ -5,7 +5,7 @@ const gltfLoader = new GLTFLoader();
 const dracoLoader = new DRACOLoader();
 const baseUrl = process.env.PUBLIC_URL;
 const loadGLTF = async () => {
-  dracoLoader.setDecoderPath("/draco/");
+  dracoLoader.setDecoderPath(baseUrl + "/draco/");
   gltfLoader.setDRACOLoader(dracoLoader);
   const houseData = await gltfLoader.loadAsync(
     baseUrl + "/models/house/LittlestTokyo.glb"
